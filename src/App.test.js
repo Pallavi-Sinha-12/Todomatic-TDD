9b120dea8b06832c950ee46fd48f0fa5a74be699
Pallavi-Sinha-12 <pallavisinha95829@gmail.com>
2022-02-09 07:53:25 +0530

    ADDED TESTS. to check if deleteTask and addTask works

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,9 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-17-updated';
 import { shallow } from 'enzyme';
 import App from './App';
-import { addTask } from './App';
-import { render } from '@testing-library/react';
+import { render, fireEvent, getByTestId} from '@testing-library/react';
 
 Enzyme.configure({ adapter: new Adapter() });
-
-//App.addTask("pallavi")
 
 it("Should render App component without crashing", () => {
   const app = shallow(<App />);
@@ -20,6 +17,25 @@ it("Should have title of Todo app as TodoMatic", () => {
     expect(app.getByTestId("app-heading")).toHaveTextContent("TodoMatic")
 })
 
-it("New test", () => {
-  expect(addTask("Pallavi"))
+it("Should be able to add task", () => {
+  const app = render(<App />);
+  fireEvent.change(app.getByTestId("new-todo"),{
+    target : {value: "new task"}
+  })
+  fireEvent.click(app.getByTestId("submit-todo"))
+  expect(app.getByTestId("todo-label").textContent).toBe(" new task ")
+
+})
+
+it("Should be able to delete task", () => {
+  const app = render(<App />);
+  fireEvent.change(app.getByTestId("new-todo"),{
+    target : {value: "new task"}
+  })
+  fireEvent.click(app.getByTestId("submit-todo"))
+  expect(app.getByTestId("todo-label").textContent).toBe(" new task ")
+
+  fireEvent.click(app.getByTestId("delete-todo"))
+  const newTodo = app.queryByText("new task")
+  expect(newTodo).toBeNull()
 })
