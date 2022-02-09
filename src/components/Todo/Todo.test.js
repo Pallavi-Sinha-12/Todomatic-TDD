090c6d0daf36012dc36aba06a8c3fed1f4c4d5e0
Pallavi-Sinha-12 <pallavisinha95829@gmail.com>
2022-02-09 07:47:05 +0530

    ADDED NEW TEST. to check label and delete button

--- src/components/Todo/Todo.test.js
+++ src/components/Todo/Todo.test.js
@@ -3,9 +3,22 @@ import Todo from './Todo';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-17-updated';
 import { shallow } from 'enzyme';
-import { render } from '@testing-library/react';
+import { render, fireEvent} from '@testing-library/react';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+let completedProps = {
+    id:1, 
+    title:"todo", 
+    completed : true,
+    deleteTask :jest.fn()
+}
+
+let notCompletedProps = {
+    id:1, 
+    title:"todo", 
+    completed : false
+}
 
 describe("Basic Rendering of Todo", () =>{
     it("Should render Todo component successfully", () => {
@@ -21,5 +34,26 @@ describe("Basic Rendering of Todo", () =>{
     it("Should be able to check or uncheck Todo to toggle task completion", () =>{
         const { getByTestId }= render(<Todo />)
         expect(getByTestId("checkbox")).not.toHaveAttribute("disabled")
+    })
+
+    it("Should have checkbox which must be checked when it is completed", () => {
+        const todo = render(<Todo {...completedProps}/>);
+        expect(todo.getByTestId("checkbox")).toBeChecked()
+    })
+
+    it("Should have checkbox which must not be checked when not completed", () => {
+        const todo = render(<Todo {...notCompletedProps}/>);
+        expect(todo.getByTestId("checkbox")).not.toBeChecked()
+    })
+
+    it("Should have label which must contain the correct label", () =>{
+        const todo = render(<Todo {...completedProps}/>);
+        expect(todo.getByTestId("todo-label").textContent).toBe(" todo ")
+    })
+
+    it("Should call delete task when button is clicked", ()=> {
+        const todo = render(<Todo {...completedProps}/>);
+        fireEvent.click(todo.getByTestId("delete-todo"))
+        expect(completedProps.deleteTask).toHaveBeenCalledTimes(1)
     })
 })
